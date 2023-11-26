22ba55a16b21d27713a3dc03b4e17a7382550853
MiraiX <117575492+666hwll@users.noreply.github.com>
2023-11-26 16:48:32 +0100

    Create conf.h

---
+++ conf.h
@@ -0,0 +1,10 @@
+#define ANSICYAN "\x1b[36m"
+#define ANSIRED  "\x1b[31m"
+#define ANSIMAGENTA "\x1b[35m"
+#define ANSIRESET "\x1b[0m"
+
+int stopwat();
+
+int alarmclock();
+
+char welcMess[] = "CCC L    O   CCC  K K\nC   L   O O  C    KK\nCCC LLL  O   CCC  K K\n";
